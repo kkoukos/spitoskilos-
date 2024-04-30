ea1d85d800e8a81e0a8e0f4ccd61830bc02ebb98
Kostas <kostaskoukos2003@gmail.com>
2024-04-30 19:10:15 +0300

    Basic Homepage Ui v0.1

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,10 @@ import LogoImg from "../../public/spitoskilos_logo_img.svg";
 export default function Home() {
   return (
     <div className="flex items-center flex-col w-full h-screen">
-      <div className="w-full bg-red-600 h-4/5 bg-no-repeat bg-cover bg-bottom ">
+      <div className="w-full bg-dog-back h-4/5 bg-no-repeat bg-cover bg-bottom ">
         {/* to outer div einai gia to image to inner gia na organonei ta pragmata */}
-        <div className="w-full bg-blue-500 flex flex-col items-center pt-10 pb-10">
-          <div className="flex justify-between w-11/12 ">
+        <div className="w-full  flex flex-col items-center pt-4">
+          <div className="flex backdrop-blur-md justify-between w-11/12  rounded-md p-5">
             <div className="flex items-center gap-2">
               <img src={LogoImg.src} className="h-12"></img>
               <img src={LogoTxt.src} className="h-8"></img>
