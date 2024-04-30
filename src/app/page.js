931535811fd85cd3ed59f7d5a0efb3d2cc5452c2
Kostas <kostaskoukos2003@gmail.com>
2024-04-30 18:14:53 +0300

    Wireframing basic hero layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,8 @@ import Image from "next/image";
 import DogBack from "../../public/dog_back.png";
 export default function Home() {
   return (
-    <div className="flex items-center flex-col w-auto h-screen">
-      <div className="w-full ">
-        <img src={DogBack.src} className="w-full h-128 "></img>
-      </div>
+    <div className="flex items-center flex-col w-full h-screen">
+      <div className="w-full bg-dog-back h-3/4 bg-no-repeat bg-cover bg-bottom"></div>
     </div>
   );
 }
