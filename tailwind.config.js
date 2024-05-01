858ffebc011ff2dc85de571c17612508808095f9
Kostas <kostaskoukos2003@gmail.com>
2024-05-01 11:08:47 +0300

    Dog animation updated. Background image temp

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "dog-back": "url('/dog_back.png')",
+        "dog-back": "url('/skilos_1.png')",
       },
       height: {
         128: "56rem",
