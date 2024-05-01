62d7f07559b77fda56ae0417d7d2dd217e4de0b6
Kostas <kostaskoukos2003@gmail.com>
2024-05-01 10:46:01 +0300

    Updated autocomplete`s lat , lng

--- src/app/kostas/autocomplete.js
+++ src/app/kostas/autocomplete.js
@@ -1,28 +1,27 @@
-
-import axios from 'axios';
+import axios from "axios";
 
 export async function autocomplete(inputText) {
-  const apiKey = 'AIzaSyC2lO3GSk-qj4gJTcFsmXP23d7oJqopuNA';
-  const url = 'https://places.googleapis.com/v1/places:autocomplete';
+  const apiKey = "AIzaSyC2lO3GSk-qj4gJTcFsmXP23d7oJqopuNA";
+  const url = "https://places.googleapis.com/v1/places:autocomplete";
 
   const requestBody = {
     input: inputText,
     locationBias: {
       circle: {
         center: {
-          latitude: 37.7749,
-          longitude: -122.4194
+          latitude: 37.98381,
+          longitude: 23.727539,
         },
-        radius: 5000.0
-      }
-    }
+        radius: 5000.0,
+      },
+    },
   };
 
   try {
     const response = await axios.post(`${url}?key=${apiKey}`, requestBody);
     return response.data;
   } catch (error) {
-    console.error('Error fetching autocomplete predictions:', error);
+    console.error("Error fetching autocomplete predictions:", error);
     throw error;
   }
 }
