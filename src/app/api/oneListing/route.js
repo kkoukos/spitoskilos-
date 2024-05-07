3637e1a57c27330336a7949e550ebd7d7d52476e
Kostas <kostaskoukos2003@gmail.com>
2024-05-07 13:56:14 +0300

    Residence Information Page and API - AlexhsV

---
+++ src/app/api/oneListing/route.js
@@ -0,0 +1,26 @@
+import clientPromise from "../../lib/db";
+import { ObjectId } from "mongodb";
+
+export async function POST(req) {
+  try {
+    const { id } = await req.json();
+
+    console.log(req.body);
+
+    const client = await clientPromise;
+    const db = client.db("listings");
+    console.log("Connected to MongoDB.");
+    console.log(id + "," + typeof id);
+
+    const data = await db.collection("Residences").findOne({ _id: id });
+    // const data = await db.collection("Residences").findOne({_id: new ObjectId("6634ff0bc2a272c8dd47c997")});
+    // console.log("Data retrieved:", data);
+    console.log(data);
+    console.log("Data received.");
+
+    return Response.json(data);
+  } catch (e) {
+    console.error("API error:", e.message);
+    return Response.json({ error: e.message });
+  }
+}
