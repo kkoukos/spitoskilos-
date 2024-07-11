aeb14855ab2bae9777825e8e3ef11997f53292fa
Kostas <kostaskoukos2003@gmail.com>
2024-07-11 16:03:23 +0300

    update 3

--- src/connectors/PlacesConnector.js
+++ src/connectors/PlacesConnector.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export async function placeConnector(inputText) {
+export async function placesConnector(inputText) {
   const apiKey = process.env.NEXT_PUBLIC_MAPS_API;
 
   const url = "https://places.googleapis.com/v1/places:autocomplete";
