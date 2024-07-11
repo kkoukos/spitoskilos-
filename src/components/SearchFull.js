503701e4332355c5729d318eac7c24e070cc0408
Kostas <kostaskoukos2003@gmail.com>
2024-07-11 16:13:56 +0300

    Update 4

--- src/components/SearchFull.js
+++ src/components/SearchFull.js
@@ -12,7 +12,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { League_Spartan } from "next/font/google";
 import { useState, useRef } from "react";
-import { placesConnector } from "../connectors/placesConnector";
+import { placesConnector } from "../connectors/placesConnector.js";
 
 const spartan_light = League_Spartan({
   subsets: ["latin"],
