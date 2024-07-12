529a9a992f823b19683cc868048d5a2914f9664f
AlexhsV <98179885+AlexhsV@users.noreply.github.com>
2024-07-12 21:27:56 +0300

    password input eye

--- src/app/listings/components/PropertyCard.js
+++ src/app/listings/components/PropertyCard.js
@@ -2,6 +2,7 @@ import BedOutlinedIcon from "@mui/icons-material/BedOutlined";
 import BathtubOutlinedIcon from "@mui/icons-material/BathtubOutlined";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Button } from "@nextui-org/react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function PropertyCard({
   property,
@@ -27,6 +28,7 @@ export default function PropertyCard({
       const { success, message } = await response.json(); // Parse JSON
       if (success) {
         setFavorites([...favorites, listing_id]);
+        toast.success("Added to favorites");
       }
       console.log(resp);
     } catch (error) {
@@ -49,6 +51,7 @@ export default function PropertyCard({
       const { success, message } = await response.json(); // Parse JSON
       if (success) {
         setFavorites(favorites.filter((id) => id !== listing_id));
+        toast.success("Removed from favorites");
       }
     } catch (error) {
       console.log("Error fetching data:", error);
@@ -56,75 +59,79 @@ export default function PropertyCard({
   };
 
   return (
-    <a
-      href={detailsUrl}
-      className="relative flex w-full bg-white shadow-lg rounded-xl mt-4 mb-4 border-1 hover:shadow-2xl transition-shadow duration-300 h-56"
-    >
-      <img
-        //   object-cover, fits the image when the window is smaller
-        className="w-1/3 object-cover rounded-l-xl"
-        src={property.pictures[0]}
-        alt="Property Photo"
-      />
-      <div className="w-2/3 p-4 ">
-        <div className="text-lg font-bold">
-          {property.propertyCategory}, {property.surface}m²
-        </div>
-        <p className=" text-sm ">{property.area}</p>
-        <p className=" text-gray-400 mt-2 truncate h-1/4 w-[90%]">
-          {property.description}
-        </p>
+    <>
+      <Toaster position="top-center" />
 
-        <div className="flex items-center text-gray-400 mt-3 space-x-4">
-          <BedOutlinedIcon className="mb-1 text-md" />
-          {property.numberOfBedrooms}
-          <BathtubOutlinedIcon className="mb-1 text-md" />
-          {property.numberOfBathrooms}
-        </div>
-        <div className="flex justify-between items-center mt-4">
-          <div className="text-xl font-bold text-gray-900">
-            {/* .toLocaleString() vazei komma "," stis xiliades*/}€
-            {property.price.toLocaleString()}
+      <a
+        href={detailsUrl}
+        className="relative flex w-full bg-white shadow-lg rounded-xl mt-4 mb-4 border-1 hover:shadow-2xl transition-shadow duration-300 h-56"
+      >
+        <img
+          //   object-cover, fits the image when the window is smaller
+          className="w-1/3 object-cover rounded-l-xl"
+          src={property.pictures[0]}
+          alt="Property Photo"
+        />
+        <div className="w-2/3 p-4 ">
+          <div className="text-lg font-bold">
+            {property.propertyCategory}, {property.surface}m²
           </div>
-          <div className="text-xs text-gray-600 mt-2">
-            Updated: {new Date(property.dateOfUpload).toLocaleDateString()}
-          </div>
-        </div>
-      </div>
-      {}
-      {!isFavorite && loggedIn && (
-        <Button
-          isIconOnly
-          color="danger"
-          variant="ghost"
-          radius="full"
-          onClick={(e) => {
-            e.preventDefault(); // Prevents the default behavior of the button
-            e.stopPropagation(); // Prevents the event from bubbling up
-            addToFavorites();
-          }}
-          className="m-2 z-10"
-        >
-          <FavoriteBorderIcon />
-        </Button>
-      )}
+          <p className=" text-sm ">{property.area}</p>
+          <p className=" text-gray-400 mt-2 truncate h-1/4 w-[90%]">
+            {property.description}
+          </p>
 
-      {isFavorite && loggedIn && (
-        <Button
-          isIconOnly
-          color="danger"
-          variant="solid"
-          radius="full"
-          onClick={(e) => {
-            e.preventDefault(); // Prevents the default behavior of the button
-            e.stopPropagation(); // Prevents the event from bubbling up
-            removeFromFavorites();
-          }}
-          className="m-2 z-10"
-        >
-          <FavoriteBorderIcon />
-        </Button>
-      )}
-    </a>
+          <div className="flex items-center text-gray-400 mt-3 space-x-4">
+            <BedOutlinedIcon className="mb-1 text-md" />
+            {property.numberOfBedrooms}
+            <BathtubOutlinedIcon className="mb-1 text-md" />
+            {property.numberOfBathrooms}
+          </div>
+          <div className="flex justify-between items-center mt-4">
+            <div className="text-xl font-bold text-gray-900">
+              {/* .toLocaleString() vazei komma "," stis xiliades*/}€
+              {property.price.toLocaleString()}
+            </div>
+            <div className="text-xs text-gray-600 mt-2">
+              Updated: {new Date(property.dateOfUpload).toLocaleDateString()}
+            </div>
+          </div>
+        </div>
+        {}
+        {!isFavorite && loggedIn && (
+          <Button
+            isIconOnly
+            color="danger"
+            variant="ghost"
+            radius="full"
+            onClick={(e) => {
+              e.preventDefault(); // Prevents the default behavior of the button
+              e.stopPropagation(); // Prevents the event from bubbling up
+              addToFavorites();
+            }}
+            className="m-2 z-10"
+          >
+            <FavoriteBorderIcon />
+          </Button>
+        )}
+
+        {isFavorite && loggedIn && (
+          <Button
+            isIconOnly
+            color="danger"
+            variant="solid"
+            radius="full"
+            onClick={(e) => {
+              e.preventDefault(); // Prevents the default behavior of the button
+              e.stopPropagation(); // Prevents the event from bubbling up
+              removeFromFavorites();
+            }}
+            className="m-2 z-10"
+          >
+            <FavoriteBorderIcon />
+          </Button>
+        )}
+      </a>
+    </>
   );
 }
